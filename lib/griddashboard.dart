caadaf4247fadf0ff0d1bb4264addb08c1857a87
sonimani292 <sonimani292@gmail.com>
2020-11-26 01:10:41 +0530

    first commit

---
+++ lib/griddashboard.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// ignore: must_be_immutable
+class GridDashboard extends StatelessWidget {
+  final String imageUrl = "assets/images/docker.png";
+  Items item1 = new Items(
+      title: "Discover",
+      subtitle: "Your Taste",
+      event: "3 Events",
+      img: "assets/images/docker.png");
+
+  Items item2 = new Items(
+    title: "Ingredients",
+    subtitle: "Just in case you want a better overview",
+    event: "4 Items",
+    img: "assets/images/docker.png",
+  );
+  Items item3 = new Items(
+    title: "Search",
+    subtitle: "",
+    event: "",
+    img: "assets/images/docker.png",
+  );
+  Items item4 = new Items(
+    title: "Favourites",
+    subtitle: "See your fav later here.",
+    event: "",
+    img: "assets/images/docker.png",
+  );
+  // Items item5 = new Items(
+  //   title: "To do",
+  //   subtitle: "Homework, Design",
+  //   event: "4 Items",
+  //   img: "assets/images/docker.png",
+  // );
+  // Items item6 = new Items(
+  //   title: "Docker",
+  //   subtitle: "",
+  //   event: "2 Items",
+  //   img: "assets/images/docker.png",
+  // );
+
+  @override
+  Widget build(BuildContext context) {
+    List<Items> myList = [item1, item2, item3, item4];
+    var color = 0xff453658;
+    return Flexible(
+      child: GridView.count(
+          childAspectRatio: 1.0,
+          padding: EdgeInsets.only(left: 16, right: 16),
+          crossAxisCount: 2,
+          crossAxisSpacing: 18,
+          mainAxisSpacing: 18,
+          children: myList.map((data) {
+            return Container(
+              decoration: BoxDecoration(
+                  color: Colors.amber[300],
+                  borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    data.img,
+                    width: 42,
+                  ),
+                  SizedBox(
+                    height: 14,
+                  ),
+                  Text(
+                    data.title,
+                    style: GoogleFonts.openSans(
+                        textStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600)),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      data.subtitle,
+                      style: GoogleFonts.openSans(
+                          textStyle: TextStyle(
+                              color: Colors.black45,
+                              fontSize: 10,
+                              fontWeight: FontWeight.w600)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 14,
+                  ),
+                  Text(
+                    data.event,
+                    style: GoogleFonts.openSans(
+                        textStyle: TextStyle(
+                            color: Colors.white70,
+                            fontSize: 11,
+                            fontWeight: FontWeight.w600)),
+                  ),
+                ],
+              ),
+            );
+          }).toList()),
+    );
+  }
+}
+
+class Items {
+  String title;
+  String subtitle;
+  String event;
+  String img;
+  Items({this.title, this.subtitle, this.event, this.img});
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:dashboard_app/griddashboard.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => new HomeState();
+}
+
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.whit,
+      appBar: AppBar(
+        title: Text('Manisha Soni'),
+        //don't change.
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {},
+          ),
+        ],
+        backgroundColor: Colors.amber[300],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 40, 20, 125),
+            child: Text('Chef Zone',
+                style: GoogleFonts.openSans(
+                    fontSize: 55, fontWeight: FontWeight.bold)),
+          ),
+          GridDashboard(),
+        ],
+      ),
+    );
+  }
+}
